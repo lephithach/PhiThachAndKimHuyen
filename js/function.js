4f53d30e24adcc66a8680eb061deca85524d0c37
Phi Thach <lephithach00@gmail.com>
2022-03-07 10:22:48 +0700

    update js date

--- js/function.js
+++ js/function.js
@@ -14,12 +14,38 @@ var DateDiff = {
   },
 };
 
-const dateInit = "2022-2-12";
+const convertDateForIos = (date) => {
+  var arr = date.split(/[- :]/);
+  date = new Date(arr[0], arr[1] - 1, arr[2], arr[3], arr[4], arr[5]);
+  return date;
+};
 
-var dateStart = new Date(dateInit);
+const formatDate = (date) => {
+  let day, month, year;
+
+  day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
+  month =
+    date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+  year = date.getFullYear();
+
+  return `${day}/${month}/${year}`;
+};
+
+const dateInit = "2022-2-12 00:00:00";
+
+var dateStart = new Date(convertDateForIos(dateInit));
 var dateEnd = new Date();
 
 document.querySelector("h2.date").innerText = `${DateDiff.inDays(
   dateStart,
   dateEnd
 )} ngày`;
+
+document.querySelector("p.week").innerText = `(${DateDiff.inWeeks(
+  dateStart,
+  dateEnd
+)} tuần)`;
+
+document.querySelector(".dateEnd").innerText = `${formatDate(
+  dateStart
+)} - ${formatDate(dateEnd)}`;

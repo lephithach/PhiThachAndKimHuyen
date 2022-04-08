adcdcc4acd7fa450ca1dbb1c47d7271866d65a38
Phi Thach <lephithach00@gmail.com>
2022-04-08 14:08:43 +0700

    update

--- js/modal.js
+++ js/modal.js
@@ -19,59 +19,66 @@ const closeModal = () => {
 
   // unlock scroll
   document.querySelector("body").style.overflowY = "scroll";
+
+  // Remove disable button zoom in
+  btnZoomIn.classList.remove("disable");
 };
 
 // function open modal
-const openModal = (e) => {
+const openModal = () => {
   modalFullImage.style.visibility = "visible";
+  modalFullImage.style.opacity = "1";
   // lock scroll
   document.querySelector("body").style.overflowY = "hidden";
+
+  // disable button zoom out
+  btnZoomOut.classList.add("disable");
+
+  handelCenterMedia();
 };
 
 btnClose.addEventListener("click", closeModal);
 modalFullImage.addEventListener("click", closeModal);
-
-modalMedia.onclick = (e) => {
+modalMedia.addEventListener("click", (e) => {
   e.stopPropagation();
-};
-
-// Show full image
-const albumImageList = document.querySelectorAll(
-  ".album-container .img-group img"
-);
-
-albumImageList.forEach((image) => {
-  image.onclick = (e) => {
-    let srcImage = e.target.getAttribute("src");
-
-    // Create div Image
-    let modalImage = document.createElement("img");
-    modalMedia.appendChild(modalImage);
-
-    modalImage.setAttribute("src", srcImage);
-
-    // Show modal
-    openModal();
-  };
 });
 
-// Show full video
-const albumVideoList = document.querySelectorAll(
-  ".album-container .img-group video"
-);
+// Show full media
+const mediaList = document.querySelectorAll(".album-container .img-group");
 
-albumVideoList.forEach((video) => {
-  video.onclick = (e) => {
-    e.preventDefault();
-    let srcVideo = e.target.getAttribute("src");
+mediaList.forEach((item) => {
+  item.onclick = (e) => {
+    // Lấy tagname của item vừa click
+    let tagName = e.target.tagName;
+    // Lấy đường dẫn của item vừa click
+    let srcMedia = e.target.getAttribute("src");
 
-    // Create div Image
-    let modalImage = document.createElement("video");
-    modalMedia.appendChild(modalImage);
+    // Tạo thẻ div theo tagname vừa click
+    let modalTagName = document.createElement(tagName);
 
-    modalImage.setAttribute("src", srcVideo);
-    modalImage.setAttribute("controls", true);
-    modalImage.play();
+    // Xử lý logic theo tagname
+    switch (tagName) {
+      case "IMG":
+        modalMedia.appendChild(modalTagName);
+
+        modalTagName.setAttribute("src", srcMedia);
+
+        break;
+
+      case "VIDEO":
+        modalMedia.appendChild(modalTagName);
+
+        modalTagName.setAttribute("src", srcMedia);
+        modalTagName.setAttribute("controls", true);
+        modalTagName.play();
+        break;
+
+      default:
+        break;
+    }
+
+    // Thêm class media để xử lý các sự kiện rotate
+    modalTagName.classList.add("media");
 
     // Show modal
     openModal();
@@ -87,48 +94,124 @@ const handelZoom = (type = "zoomIn", zoom, number) => {
   number = Number(number);
 
   if (type === "zoomIn") {
-    if (zoom >= 10) return 10;
+    if (zoom >= 10) {
+      btnZoomIn.classList.add("disable");
+      return 10;
+    }
     zoom += number;
   } else {
-    if (zoom <= 0.4) return 0.4;
+    if (zoom == 1) {
+      btnZoomOut.classList.add("disable");
+      return 1;
+    }
     zoom -= number;
   }
 
   return zoom.toFixed(1);
 };
 
-// Zoom in
-btnZoomIn.onclick = (e) => {
-  e.stopPropagation();
-  let sel = modalMedia.querySelector("img");
-  let scale = 0;
-
-  if (!sel.getAttribute("data-zoom")) {
-    let initZoom = 1;
-    scale = handelZoom("zoomIn", initZoom, 0.6);
-  } else {
-    let initZoom = sel.getAttribute("data-zoom");
-    scale = handelZoom("zoomIn", initZoom, 0.6);
-  }
-
-  sel.setAttribute("style", `transform: scale(${scale});`);
-  sel.setAttribute("data-zoom", `${scale}`);
+const handelDragMedia = (status, element) => {
+  status
+    ? new Draggabilly(document.querySelector(".modal.full-image .media"))
+    : handelCenterMedia();
 };
 
-// Zoom out
-btnZoomOut.onclick = (e) => {
+const handelCenterMedia = () => {
+  modalMedia.style = "";
+};
+
+// Zoom in
+btnZoomIn.addEventListener("click", (e) => {
   e.stopPropagation();
-  let sel = modalMedia.querySelector("img");
+  let mediaElement = modalMedia.querySelector("img");
   let scale = 0;
 
-  if (!sel.getAttribute("data-zoom")) {
+  if (!mediaElement.getAttribute("data-zoom")) {
     let initZoom = 1;
-    scale = handelZoom("zoomOut", initZoom, 0.6);
+    scale = handelZoom("zoomIn", initZoom, 0.6);
   } else {
-    let initZoom = sel.getAttribute("data-zoom");
-    scale = handelZoom("zoomOut", initZoom, 0.6);
+    let initZoom = mediaElement.getAttribute("data-zoom");
+    scale = handelZoom("zoomIn", initZoom, 0.6);
   }
 
-  sel.setAttribute("style", `transform: scale(${scale});`);
-  sel.setAttribute("data-zoom", `${scale}`);
+  // Change CSS image zoom
+  mediaElement.setAttribute("style", `transform: scale(${scale});`);
+  // Set attr image
+  mediaElement.setAttribute("data-zoom", `${scale}`);
+
+  // Remove disable zoom out
+  btnZoomOut.classList.remove("disable");
+
+  // Drag image media
+  handelDragMedia(true, modalMedia);
+});
+
+// Zoom out
+btnZoomOut.addEventListener("click", (e) => {
+  e.stopPropagation();
+  let mediaElement = modalMedia.querySelector("img");
+  let scale = 0;
+
+  // if (!sel.getAttribute("data-zoom")) {
+  //   let initZoom = 1;
+  //   scale = handelZoom("zoomOut", initZoom, 0.6);
+  // } else {
+  //   let initZoom = sel.getAttribute("data-zoom");
+  //   scale = handelZoom("zoomOut", initZoom, 0.6);
+  // }
+
+  if (mediaElement.getAttribute("data-zoom")) {
+    let initZoom = mediaElement.getAttribute("data-zoom");
+    scale = handelZoom("zoomOut", initZoom, 0.6);
+
+    // Change CSS image zoom
+    mediaElement.setAttribute("style", `transform: scale(${scale});`);
+    // Set attr image
+    mediaElement.setAttribute("data-zoom", `${scale}`);
+
+    if (scale == 1) {
+      // Drag image media
+      handelDragMedia(false, modalMedia);
+    }
+
+    // Remove disable zoom in
+    btnZoomIn.classList.remove("disable");
+  }
+});
+
+// Rotate
+const btnRotateRight = document.querySelector(".tools .rotate-right");
+const btnRotateLeft = document.querySelector(".tools .rotate-left");
+
+btnRotateRight.addEventListener("click", (e) => {
+  e.stopPropagation();
+  handelRotate("right", modalMedia);
+});
+
+btnRotateLeft.addEventListener("click", (e) => {
+  e.stopPropagation();
+  handelRotate("left", modalMedia);
+});
+
+const handelRotate = (direction, element) => {
+  element = element.querySelector(".media");
+  let deg = Number(element.getAttribute("rotate"));
+
+  switch (direction) {
+    case "right":
+      deg = deg == 0 ? 90 : (deg += 90);
+      break;
+
+    case "left":
+      deg = deg == 0 ? -90 : (deg -= 90);
+      break;
+
+    default:
+      element.style.transform = "rotate(0)";
+      element.setAttribute("rotate", 0);
+      break;
+  }
+
+  element.style.transform = `rotate(${deg}deg)`;
+  element.setAttribute("rotate", deg);
 };
--- js/render.js
+++ js/render.js
@@ -30,7 +30,7 @@ var data = dataArr.map((item, key) => {
 
     case "video":
       html += `
-          <div class="img-group wow animate__animated ${randomClassAnimate}">
+          <div class="img-group video wow animate__animated ${randomClassAnimate}">
               <p class="title">${item.title}</p>
               <video
                 src="${item.video_url}"
@@ -39,6 +39,10 @@ var data = dataArr.map((item, key) => {
                 title="${item.alt}"
                 data-id="${key + 1}"
               ></video>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"/>
+</svg>
           </div>
       `;
 
